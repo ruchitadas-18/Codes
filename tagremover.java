665ad8260f1c16974120a13b81852394606cc366
Ruchita Das <80640169+ruchitadas-18@users.noreply.github.com>
2023-04-20 09:14:11 +0530

    Create tagremover.java

---
+++ tagremover.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution{
+    public static void main(String[] args) {
+        String regex = "<(.+)>([^<>]+)</(\\1)>";
+        Pattern p = Pattern.compile(regex);
+
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while (testCases > 0) {
+            String line = in.nextLine();
+
+        // Write your code here
+            Matcher m = p.matcher(line);
+
+            boolean bandera = true;
+            while (m.find()) {
+                System.out.println(m.group(2));
+                bandera = false;
+            }
+            if (bandera) {
+            System.out.println("None");
+        }
+
+        testCases--;
+    }
+}
