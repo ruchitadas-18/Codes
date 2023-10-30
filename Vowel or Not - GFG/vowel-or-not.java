27adc500d5082daabbf6082634e96dbb4c73d1e0
Ruchita Das <80640169+ruchitadas-18@users.noreply.github.com>
2023-10-30 22:37:24 +0530

    Added solution - LeetHub

---
+++ Vowel or Not - GFG/vowel-or-not.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    static class FastReader{ 
+		BufferedReader br; 
+		StringTokenizer st; 
+  
+		public FastReader(){ 
+			br = new BufferedReader(new InputStreamReader(System.in)); 
+		} 
+  
+		String next(){ 
+			while (st == null || !st.hasMoreElements()){ 
+				try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+			} 
+			return st.nextToken(); 
+		} 
+  
+		String nextLine(){ 
+			String str = ""; 
+			try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+			return str; 
+		} 
+	}
+	
+    public static void main(String args[])throws IOException
+    {
+        FastReader sc = new FastReader();
+        int t = Integer.parseInt(sc.next());
+        while(t-- > 0)
+        {
+            char c = sc.next().charAt(0); 
+
+            Solution ob = new Solution();
+            System.out.println(ob.isVowel(c));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    String isVowel(char c){
+        // code here
+        String result = " "; 
+        if(c =='a' || c == 'A')
+            result = "YES";
+        else if(c=='e' || c == 'E')
+            result = "YES";
+        else if(c=='i' || c =='I')
+            result ="YES";
+        else if (c =='o'||c=='O')
+            result ="YES";
+        else if(c=='u'||c=='U')
+            result = "YES";
+        else
+            result = "NO";
+        
+        return result;
+    }
+}
